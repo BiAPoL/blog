17ed043c4f13a2452b025e2ca1493cb1577573c8
Marcelo Zoccoler <marcelozoc@gmail.com>
2021-11-29 18:14:54 +0100

    added figure5

--- marcelo_zoccoler/entry_user_interf3/scripts/functiongui_add_flood_tool.py
+++ marcelo_zoccoler/entry_user_interf3/scripts/functiongui_add_flood_tool.py
@@ -6,13 +6,10 @@ from skimage.io import imread
 from magicgui.widgets import FunctionGui
 from napari.types import ImageData, LabelsData, LayerDataTuple
 
-def flood(image: ImageData, delta: float=0, level: int=0) -> LayerDataTuple: 
+def flood(image: ImageData, delta: float=0, new_level: int=0) -> LayerDataTuple: 
     new_level = delta*85
-    
     label_image = image <= new_level
-    
     label_image = label_image.astype(int)*13 # label 13 is blue in napari
-
     return((label_image, {'name': 'flood result','metadata': {'new_level':new_level}}))
 
 class MyGui(FunctionGui):
@@ -24,30 +21,30 @@ class MyGui(FunctionGui):
           param_options={'delta':
                              {'label': 'Temperature Increase (Δ°C):', 
                               'min': 0, 'max' : 3, 'step': 0.1},
-                        'level':
-                            {'label':'Water Level:', 'widget_type':'Slider',
+                        'new_level':
+                            {'label':'Sea Level (dm):', 'widget_type':'Slider',
                              'min': 0, 'max' : 255}}
         )
         
     def __call__(self):
         label_image = super().__call__()
         new_level = round(label_image[1]['metadata']['new_level'])
-        self.level.value = new_level
+        self.new_level.value = new_level
 
 
 napari_image = imread('../images/21_Map_of_Tabuaeran_Kiribati_blue.png')
 viewer = napari.Viewer()
 viewer.add_image(napari_image, name='napari_island')#, colormap='viridis')   
 
-# flood_widget = MyGui()
+flood_widget = MyGui()
 
-# dw_instance = viewer.window.add_dock_widget(flood_widget, area='right')
+viewer.window.add_dock_widget(flood_widget, area='right')
 napari.run()  # start the event loop and show viewer
 
 
 
 
-from napari_plugin_engine import napari_hook_implementation
+# from napari_plugin_engine import napari_hook_implementation
 
 # @napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
 # def main_dock_widget():
@@ -55,6 +52,6 @@ from napari_plugin_engine import napari_hook_implementation
 #     return MyGui
 
 
-@napari_hook_implementation
-def napari_experimental_provide_dock_widget():
-    return MyGui
+# @napari_hook_implementation
+# def napari_experimental_provide_dock_widget():
+#     return MyGui
