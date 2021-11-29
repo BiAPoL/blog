fda809de76aa1a524c41e47f9b1a93daa81c7d57
Marcelo Zoccoler <marcelozoc@gmail.com>
2021-11-29 14:13:56 +0100

    added new images related to magicgui implementation

--- marcelo_zoccoler/entry_user_interf3/scripts/add_flood_tool.py
+++ marcelo_zoccoler/entry_user_interf3/scripts/add_flood_tool.py
@@ -1,6 +1,4 @@
 import napari
-
-# This imports the previously generated UI file
 from flood_tool import Ui_MainWindow
 from skimage.io import imread
 from PyQt5.QtWidgets import QMainWindow
@@ -13,33 +11,27 @@ def flood(image, delta):
 
 # Define the main window class
 class MainWindow(QMainWindow,  Ui_MainWindow):
-    def __init__(self, napari_viewer):
+    def __init__(self, napari_viewer):          # include napari_viewer as argument (it has to have this name)
         super().__init__()
         self.viewer = napari_viewer
-        #Initialize GUI
-        self.setupUi(self)
+        self.setupUi(self)                     # Initialize GUI
         
-        self.label_layer = None
+        self.label_layer = None                # stored label layer variable
         self.pushButton.clicked.connect(self._apply_delta)
-        
-    def closeEvent(self, event):
-        self.close()
     
     def _apply_delta(self):
-        image = self.viewer.layers['napari_island'].data
+        image = self.viewer.layers['napari_island'].data    # We use the layer name to find the correct image layer
         delta = self.doubleSpinBox.value()
         label, level = flood(image, delta)
         if self.label_layer is None:
-            self.label_layer = self.viewer.add_labels(label,opacity = 1)
+            self.label_layer = self.viewer.add_labels(label)
         else:
             self.label_layer.data = label
         self.horizontalSlider.setValue(level)
 
-napari_image = imread('../images/21_Map_of_Tabuaeran_Kiribati_blue.png')
 viewer = napari.Viewer()
-viewer.add_image(napari_image, name='napari_island')  
+napari_image = imread('../images/21_Map_of_Tabuaeran_Kiribati_blue.png')    # Reads an image from file
+viewer.add_image(napari_image, name='napari_island')                       # Adds the image to the viewer and give the image layer a name
 
-flood_widget = MainWindow(viewer)
-
-dw_instance = viewer.window.add_dock_widget(flood_widget, area='right')
-# napari.run()  # start the event loop and show viewer
+flood_widget = MainWindow(viewer)                                          # Create instance from our class
+viewer.window.add_dock_widget(flood_widget, area='right')                  # Add our gui instance to napari viewer
--- marcelo_zoccoler/entry_user_interf3/scripts/magicgui_add_flood_tool.py
+++ marcelo_zoccoler/entry_user_interf3/scripts/magicgui_add_flood_tool.py
@@ -1,28 +1,18 @@
 import napari
-# This imports the previously generated UI file
 from skimage.io import imread
-
 from magicgui import magicgui
 from napari.types import ImageData, LabelsData
 
-def flood(image: ImageData, delta: float=0, level: int=0) -> LabelsData: 
+def flood(image: ImageData, delta: float=0, new_level: int=0) -> LabelsData: 
     new_level = delta*85
-    
     label_image = image <= new_level
-    
     label_image = label_image.astype(int)*13 # label 13 is blue in napari
-
     return(label_image)
 
-napari_image = imread('../images/21_Map_of_Tabuaeran_Kiribati_blue.png')
 viewer = napari.Viewer()
-viewer.add_image(napari_image, name='napari_island')#, colormap='viridis')   
+napari_image = imread('../images/21_Map_of_Tabuaeran_Kiribati_blue.png')    # Reads an image from file
+viewer.add_image(napari_image, name='napari_island')                       # Adds the image to the viewer and give the image layer a name 
 
-flood_widget = magicgui(flood, delta={'label': 'Temperature Increase (Δ°C):', 
-                                           'min': 0, 'max' : 3, 'step': 0.1},
-                                    level={'label':'Water Level:', 'widget_type':'Slider',
-                                           'min': 0, 'max' : 255})
-
-dw_instance = viewer.window.add_dock_widget(flood_widget, area='right')
-napari.run()  # start the event loop and show viewer
+flood_widget = magicgui(flood)                                             # Create GUI with magicgui
+viewer.window.add_dock_widget(flood_widget, area='right')                  # Add our gui instance to napari viewer
 
